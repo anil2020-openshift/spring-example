ac528329779eeda52b2629d6789d855975f8a296
anil2020-openshift <69211433+anil2020-openshift@users.noreply.github.com>
2020-08-09 13:10:08 +0530

    a
    a

--- src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
+++ src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
@@ -12,7 +12,7 @@ public class SpringExampleApplication {
 
 	@GetMapping("/")
 	public String welcome() {
-		return "Welcome to java techie";
+		return "Welcome to java techie: Openshift";
 	}
 
 	@GetMapping("/{input}")
